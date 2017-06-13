75d497732ea6e65579bb83de8df8f3b0dd06e2ea
yinm <yinm.cminor9@gmail.com>
2017-06-14 07:33:55 +0900

    スコープチェーンの例

--- chapter2/scripts.js
+++ chapter2/scripts.js
@@ -1,10 +1,16 @@
-let regular_joe = 'regular joe is assigned';
+let regular_joe = 'I am here to save the day!';
+console.log(regular_joe);
 
-function prison ( regular_joe ) {
+function supermax () {
+  let regular_joe = 'regular_joe is assigned';
   console.log(regular_joe);
-  let regular_joe;
 
-  console.log(regular_joe);
+  function prison () {
+    let regular_joe;
+    console.log(regular_joe);
+  }
+
+  prison();
 }
 
-prison( 'the regular_joe argument' );
+supermax();
