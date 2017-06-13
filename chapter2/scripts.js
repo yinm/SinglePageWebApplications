2eafaf8083b3f66c4efa04b1dba88730fe8a00bc
yinm <yinm.cminor9@gmail.com>
2017-06-14 06:44:53 +0900

    引数の値が先に評価される例
    letを使うと、すでに宣言済みであるエラーになるみたい

--- chapter2/scripts.js
+++ chapter2/scripts.js
@@ -1,8 +1,10 @@
-let regular_joe = 'regular_joe is assigned';
+let regular_joe = 'regular joe is assigned';
 
-function prison () {
+function prison ( regular_joe ) {
   console.log(regular_joe);
   let regular_joe;
+
+  console.log(regular_joe);
 }
 
-prison();
+prison( 'the regular_joe argument' );
