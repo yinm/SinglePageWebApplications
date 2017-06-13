71dc4fa57111cbd3bd6c47e4ec1ea7591fdc006c
yinm <yinm.cminor9@gmail.com>
2017-06-14 08:19:03 +0900

    無名関数に引数を渡す

--- chapter2/scripts.js
+++ chapter2/scripts.js
@@ -1,22 +1,4 @@
-let proto = {
-  sentence  : 4,
-  probation : 2
-};
-
-let makePrisoner = function(name, id) {
-  let prisoner = Object.create(proto);
-  prisoner.name = name;
-  prisoner.id = id;
-
-  return prisoner;
-};
-
-let firstPrisoner = makePrisoner('Joe', '12A');
-let secondPrisoner = makePrisoner('Sam', '2BC');
-
-console.log(firstPrisoner.sentence);
-console.log(secondPrisoner.sentence);
-
-proto.sentence = 5;
-console.log(firstPrisoner.sentence);
-console.log(secondPrisoner.sentence);
+(function (what_to_eat) {
+  let sentence = 'I am going to eat a ' + what_to_eat;
+  console.log(sentence);
+})('sandwhich');
