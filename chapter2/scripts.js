78f00a47ae382f97e5fa7d0e848b6178c7b7ea52
yinm <yinm@pepabo.com>
2017-06-14 09:45:58 +0900

    自己実行型無名関数と、関数定義型の場合の比較

--- chapter2/scripts.js
+++ chapter2/scripts.js
@@ -1,22 +1,11 @@
-let proto = {
-  sentence  : 4,
-  probation : 2
-};
+let prison = (function() {
+  return 'Mike is in prison';
+})();
 
-let makePrisoner = function ( name, id ) {
-  let prisoner = Object.create(proto);
-  prisoner.name = name;
-  prisoner.id = id;
-  return prisoner;
-};
+function makePrison() {
+  return 'Mike is in prison';
+}
+let prison2 = makePrison();
 
-let firstPrisoner = makePrisoner('Joe', '12A');
-console.log(firstPrisoner);
-
-console.log(firstPrisoner.__proto__);
-
-console.log(firstPrisoner.__proto__.__proto__);
-
-console.log(firstPrisoner.__proto__.__proto__.__proto__);
-
-console.log(firstPrisoner.__proto__.__proto__.__proto__.__proto__);
+console.log(prison);
+console.log(prison2);
