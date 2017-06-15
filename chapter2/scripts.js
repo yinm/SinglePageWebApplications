2bb8402f8d1f064a0ddd9013297a890c69c87a88
yinm <yinm.cminor9@gmail.com>
2017-06-15 22:09:36 +0900

    スコープチェーンの例 (近くに変数がなければ、グローバルオブジェクトまで辿る)

--- chapter2/scripts.js
+++ chapter2/scripts.js
@@ -1,17 +1,13 @@
-let regular_joe = 'I am here to save the day!';
-
+let regular_joe = 'I am here to save the day';
 console.log(regular_joe);
 
 function supermax() {
-  let regular_joe = 'regular_joe is assigned';
-
   console.log(regular_joe);
 
   function prison() {
-    let regular_joe;
     console.log(regular_joe);
   }
-  prison()
+  prison();
 
 }
 supermax();
