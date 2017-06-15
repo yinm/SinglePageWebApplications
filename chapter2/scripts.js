8828ef314aa5e9161d5caaa6877a5849c03a44fb
yinm <yinm.cminor9@gmail.com>
2017-06-16 08:22:33 +0900

    無名関数の戻り値を使う

--- chapter2/scripts.js
+++ chapter2/scripts.js
@@ -1,5 +1,14 @@
-function makePrison() {
-  return 'Mike is in prison';
-}
-let prison = makePrison();
-console.log(prison);
+let prison = (function() {
+  const
+    prisoner_name = 'Mike Mikowski',
+    jail_term = '20 year term';
+
+  return {
+    prisoner: prisoner_name + ' - ' + jail_term,
+    sentence: jail_term
+  };
+})();
+
+console.log(prison.prisoner_name);
+console.log(prison.prisoner);
+console.log(prison.sentence);
