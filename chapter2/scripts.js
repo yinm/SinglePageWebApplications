0c52f3c5aff4c8b71c4d6aa06f62744fe2b9ab70
yinm <yinm.cminor9@gmail.com>
2017-06-16 08:18:23 +0900

    変数代入時に即時実行する関数

--- chapter2/scripts.js
+++ chapter2/scripts.js
@@ -1,4 +1,4 @@
-(function(what_to_eat) {
-  const sentence = 'I am going to eat a ' + what_to_eat;
-  console.log(sentence);
-})('sandwich');
+let prison = (function() {
+  return 'Mike is in prison';
+})();
+console.log(prison);
