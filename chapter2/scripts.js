aa06bf912835fed6481343d7e1bb5d2bf7c840c1
yinm <yinm.cminor9@gmail.com>
2017-06-16 08:04:54 +0900

    無名関数でもう一度書く

--- chapter2/scripts.js
+++ chapter2/scripts.js
@@ -1,5 +1,4 @@
-let eatFunction = function(what_to_eat) {
-  let sentence = 'I am going to eat a ' + what_to_eat;
+(function(what_to_eat) {
+  const sentence = 'I am going to eat a ' + what_to_eat;
   console.log(sentence);
-};
-eatFunction('sandwich');
+})('sandwich');
