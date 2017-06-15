c611d83842461bcffa0cc0f5cc12dba5ed6ee0b7
yinm <yinm.cminor9@gmail.com>
2017-06-16 07:47:37 +0900

    無名関数を、変数に格納して使用する

--- chapter2/scripts.js
+++ chapter2/scripts.js
@@ -1,22 +1,4 @@
-let proto = {
-  sentence: 4,
-  probation: 2
+let prison = function() {
+  console.log('prison called');
 };
-
-let makePrisoner = function(name, id) {
-  let prisoner = Object.create(proto);
-  prisoner.name = name;
-  prisoner.id = id;
-
-  return prisoner;
-};
-
-let firstPrisoner = makePrisoner('Joe', '12A');
-let secondPrisoner = makePrisoner('Sam', '2BC');
-
-console.log(firstPrisoner.sentence);
-console.log(secondPrisoner.sentence);
-
-proto.sentence = 5;
-console.log(firstPrisoner.sentence);
-console.log(secondPrisoner.sentence);
+prison();
