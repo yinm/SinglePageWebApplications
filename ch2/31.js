3eb41fa6c6f6e12f64451d22b4c1fbc736024958
yinm <yinm.cminor9@gmail.com>
2018-02-12 22:28:16 +0900

    Check closure

---
+++ ch2/31.js
@@ -0,0 +1,12 @@
+var prison = {
+  names: 'Mike Mikowski and Josh Powell',
+  who: function() {
+    var that = this;
+
+    setTimeout(function() {
+      console.log(that.names);
+    }, 1000);
+  }
+};
+
+prison.who();
