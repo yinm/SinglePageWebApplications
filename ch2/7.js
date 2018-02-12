061682d771b53e0a7b8da48d76c8390ee34f651a
yinm <yinm.cminor9@gmail.com>
2018-02-12 20:47:52 +0900

    Check hoist

---
+++ ch2/7.js
@@ -0,0 +1,10 @@
+var regular_joe = 'regular_joe is assigned';
+
+function prison(regular_joe) {
+  console.log(regular_joe);
+  var regular_joe;
+
+  console.log(regular_joe);
+}
+
+prison('the regular_joe argument');
