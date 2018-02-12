73c28556ab41687ae8308c6f13c7ebd19d56d204
yinm <yinm.cminor9@gmail.com>
2018-02-12 21:20:45 +0900

    Check prototype with factory method

---
+++ ch2/13.js
@@ -0,0 +1,18 @@
+var proto = {
+  sentence: 4,
+  probation: 2
+};
+
+var makePrisoner = function(name, id) {
+  var prisoner = Object.create(proto);
+  prisoner.name = name;
+  prisoner.id = id;
+
+  return prisoner;
+};
+
+var firstPrisoner = makePrisoner('Joe', '12A');
+var secondPrisoner = makePrisoner('Sam', '2BC');
+
+console.log(firstPrisoner);
+console.log(secondPrisoner);
