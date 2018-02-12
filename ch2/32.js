ba0385a45a793f21bc9b4ccb0cd4b49bc3877138
yinm <yinm.cminor9@gmail.com>
2018-02-12 22:32:26 +0900

    Check closure

---
+++ ch2/32.js
@@ -0,0 +1,11 @@
+var makePrison = function(prisoner) {
+  return function() {
+    return prisoner;
+  }
+};
+
+var joshPrison = makePrison('Josh Powell');
+var mikePrison = makePrison('Mike Mikowski');
+
+console.log(joshPrison());
+console.log(mikePrison());
