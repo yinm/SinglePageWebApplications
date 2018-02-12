f3e8c6de931a5527e794c724e8db2169282ea69e
yinm <yinm.cminor9@gmail.com>
2018-02-12 21:33:19 +0900

    Check overwriting prototype

---
+++ ch2/15.js
@@ -0,0 +1,25 @@
+var proto = {
+  sentence: 4,
+  probation: 2
+};
+
+var makePrisoner = function(name, id) {
+  var prisoner = Object.create(proto);
+  prisoner.name = name;
+  prisoner.id = id;
+
+  return prisoner;
+};
+
+var firstPrisoner = makePrisoner('Joe', '12A');
+
+console.log(firstPrisoner.sentence);
+console.log(firstPrisoner.__proto__.sentence);
+
+firstPrisoner.sentence = 10;
+console.log(firstPrisoner.sentence);
+console.log(firstPrisoner.__proto__.sentence);
+
+delete firstPrisoner.sentence;
+console.log(firstPrisoner.sentence);
+console.log(firstPrisoner.__proto__.sentence);
