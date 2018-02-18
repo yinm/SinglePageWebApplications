012561c08f325d9b5020b2e3d434f057779242f4
yinm <yinm.cminor9@gmail.com>
2018-02-18 09:25:53 +0900

    Check String.prototype.replace()

---
+++ Appendix/07.js
@@ -0,0 +1,12 @@
+// [String.prototype.replace() - JavaScript | MDN](https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/replace)
+
+function styleHyphenFormat(propertyName) {
+  function upperToHyphenLower(match, offset, string) {
+    return (offset > 0 ? '-' : '') + match.toLowerCase();
+  }
+
+  return propertyName.replace(/[A-Z]/g, upperToHyphenLower);
+}
+
+console.log(styleHyphenFormat('borderTop'));
+console.log(styleHyphenFormat('BorderTop'));
