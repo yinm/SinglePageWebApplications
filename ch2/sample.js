ca026c3d7065e37828a1845d2ad72e700b9faccc
yinm <yinm.cminor9@gmail.com>
2018-12-21 09:37:39 +0900

    Check hoist

--- ch2/sample.js
+++ ch2/sample.js
@@ -1,5 +1,9 @@
 function prison () {
   console.log(prisoner);
-}
+  var prisoner, warden, guards;
 
+  console.log(prisoner)
+  prisoner = 'prisoner assigned';
+  console.log(prisoner);
+}
 prison();
