e4f7085293bea9dc836d246138c86fedfdbd8eb3
yinm <yinm.cminor9@gmail.com>
2018-12-22 08:37:03 +0900

    Check scope chains

--- ch2/sample.js
+++ ch2/sample.js
@@ -1,9 +1,14 @@
-function prison () {
-  console.log(prisoner);
-  var prisoner, warden, guards;
+var regular_joe = 'I am here to save the day!';
 
-  console.log(prisoner)
-  prisoner = 'prisoner assigned';
-  console.log(prisoner);
+console.log(regular_joe);
+function supermax() {
+  var regular_joe = 'regular_joe is assigned';
+  console.log(regular_joe);
+
+  function prison () {
+    var regular_joe;
+    console.log(regular_joe);
+  }
+  prison();
 }
-prison();
+supermax();
