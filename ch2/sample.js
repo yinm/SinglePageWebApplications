d18294113c086f7e8d1b9ff1d94ae9c08877670f
yinm <yinm.cminor9@gmail.com>
2018-12-22 08:38:57 +0900

    Check scope chains

--- ch2/sample.js
+++ ch2/sample.js
@@ -2,13 +2,11 @@ var regular_joe = 'I am here to save the day!';
 
 console.log(regular_joe);
 function supermax() {
-  var regular_joe = 'regular_joe is assigned';
   console.log(regular_joe);
-
-  function prison () {
-    var regular_joe;
+  function prison() {
     console.log(regular_joe);
   }
+
   prison();
 }
 supermax();
