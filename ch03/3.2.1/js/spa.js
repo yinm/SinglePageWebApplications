1f6b6a488f438f9ec398fe12e774f6c22471faa8
yinm <yinm.cminor9@gmail.com>
2018-12-22 19:51:29 +0900

    Add js

---
+++ ch03/3.2.1/js/spa.js
@@ -0,0 +1,11 @@
+var spa = (function () {
+  var initModule = function ($container) {
+    $container.html(
+      '<h1 style="display:inline-block;" margin:25px;">'
+        + 'hello world!'
+      + '</h1>'
+    )
+  }
+
+  return { initModule: initModule }
+})()
