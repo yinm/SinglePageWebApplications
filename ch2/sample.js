6c0517d537799a0e3beccac9258680368cbd3a52
yinm <yinm.cminor9@gmail.com>
2018-12-22 09:09:07 +0900

    Check function scopes

--- ch2/sample.js
+++ ch2/sample.js
@@ -1,23 +1,7 @@
-var proto = {
-  sentence: 4,
-  probation: 2
+var myApplication = function() {
+  var private_variable = 'private'
 }
 
-var makePrisoner = function(name, id) {
-  var prisoner = Object.create(proto)
-  prisoner.name = name
-  prisoner.id = id
+myApplication()
 
-  return prisoner
-}
-
-var firstPrisoner = makePrisoner('Joe', '12A')
-var secondPrisoner = makePrisoner('Sam', '2BC')
-
-console.log(firstPrisoner.sentence)
-console.log(secondPrisoner.sentence)
-
-proto.sentence = 5
-
-console.log(firstPrisoner.sentence)
-console.log(secondPrisoner.sentence)
+console.log(private_variable)
