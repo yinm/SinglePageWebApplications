190c1cb2452fcfde4be71aefb9b063f867ea8d20
yinm <yinm.cminor9@gmail.com>
2018-12-22 09:23:01 +0900

    Check module patterns with args

--- ch2/sample.js
+++ ch2/sample.js
@@ -1,4 +1,10 @@
+var eatFunction = function (what_to_eat) {
+  var sentence = 'I am going to eat a ' + what_to_eat
+  console.log(sentence)
+}
+eatFunction('sandwich');
+
 (function(what_to_eat) {
-  var sentence = 'I am going to eat a ' + what_to_eat;
-  console.log(sentence);
+  var sentence = 'I am going to eat a ' + what_to_eat
+  console.log(sentence)
 })('sandwich')
