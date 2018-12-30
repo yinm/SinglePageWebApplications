825e77f1a3980a9fd0addc7534e4e3df92c076b8
yinm <yinm.cminor9@gmail.com>
2018-12-31 08:54:53 +0900

    Check routings

---
+++ ch07/7.2.2.3-rude-router/app.js
@@ -0,0 +1,12 @@
+var http, server
+
+http = require('http')
+server = http.createServer(function(request, response) {
+  var response_text = request.url === '/test'
+    ? 'you have hit the test page'
+    : 'Hello World'
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end(response_text)
+}).listen(3000)
+
+console.log('Listening on port %d', server.address().port)
