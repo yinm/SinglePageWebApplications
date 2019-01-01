5a5f3e5026647091819cd6bc64553d5de772c3f2
yinm <yinm.cminor9@gmail.com>
2019-01-01 09:54:14 +0900

    Add routes of update and delete

--- ch07/7.3.1.1-routing/app.js
+++ ch07/7.3.1.1-routing/app.js
@@ -41,6 +41,20 @@ app.get('/user/read/:id([0-9]+)', function (request, response) {
   })
 })
 
+app.post('/user/update/:id([0-9]+)', function(request, response) {
+  response.contentType('json')
+  response.send({
+    title: 'user with id ' + request.params.id + ' updated'
+  })
+})
+
+app.get('/user/delete/:id([0-9]+)', function(request, response) {
+  response.contentType('json')
+  response.send({
+    title: 'user with id ' + request.params.id + ' deleted'
+  })
+})
+
 server.listen(3000)
 console.log(
   'Express server listening on port %d in %s mode',
