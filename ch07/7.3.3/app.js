2f646584a8124f464e2142696d543a8fa15d650b
yinm <yinm.cminor9@gmail.com>
2019-01-01 10:20:26 +0900

    Use a module of routing

--- ch07/7.3.3/app.js
+++ ch07/7.3.3/app.js
@@ -12,8 +12,6 @@ var
   app = express(),
   server = http.createServer(app)
 
-routes()
-
 app.use(bodyParser.json())
 app.use(methodOverride('X-HTTP-Method-Override'))
 app.use(express.static(__dirname + '/public'))
@@ -23,40 +21,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(logger('combined'))
 }
 
-app.get('/', function (request, response) {
-  response.redirect('/spa.html')
-})
-
-app.all('/:obj_type/*?', function(request, response, next) {
-  response.contentType('json')
-  next()
-})
-
-app.get('/:obj_type/list', function (request, response) {
-  response.send({ title: request.params.obj_type + ' list' })
-})
-
-app.post('/:obj_type/create', function (request, response) {
-  response.send({ title: request.params.obj_type + ' created' })
-})
-
-app.get('/:obj_type/read/:id([0-9]+)', function (request, response) {
-  response.send({
-    title: request.params.obj_type + ' with id ' + request.params.id + ' found'
-  })
-})
-
-app.post('/:obj_type/update/:id([0-9]+)', function(request, response) {
-  response.send({
-    title: request.params.obj_type + ' with id ' + request.params.id + ' updated'
-  })
-})
-
-app.get('/:obj_type/delete/:id([0-9]+)', function(request, response) {
-  response.send({
-    title: request.params.obj_type + ' with id ' + request.params.id + ' deleted'
-  })
-})
+routes.configRoutes(app, server)
 
 server.listen(3000)
 console.log(
