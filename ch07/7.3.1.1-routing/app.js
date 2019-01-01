bc44a8257232c3a778efd528420b6f6c9a608a65
yinm <yinm.cminor9@gmail.com>
2019-01-01 09:56:33 +0900

    Use all method

--- ch07/7.3.1.1-routing/app.js
+++ ch07/7.3.1.1-routing/app.js
@@ -24,32 +24,32 @@ app.get('/', function (request, response) {
   response.redirect('/spa.html')
 })
 
-app.get('/user/list', function (request, response) {
+app.all('/user/*?', function(request, response, next) {
   response.contentType('json')
+  next()
+})
+
+app.get('/user/list', function (request, response) {
   response.send({ title: 'user list' })
 })
 
 app.post('/user/create', function (request, response) {
-  response.contentType('json')
   response.send({ title: 'user created' })
 })
 
 app.get('/user/read/:id([0-9]+)', function (request, response) {
-  response.contentType('json')
   response.send({
     title: 'user with id ' + request.params.id + ' found'
   })
 })
 
 app.post('/user/update/:id([0-9]+)', function(request, response) {
-  response.contentType('json')
   response.send({
     title: 'user with id ' + request.params.id + ' updated'
   })
 })
 
 app.get('/user/delete/:id([0-9]+)', function(request, response) {
-  response.contentType('json')
   response.send({
     title: 'user with id ' + request.params.id + ' deleted'
   })
