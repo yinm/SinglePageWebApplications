1f4980946ec454db0bcbd0076fc865ff85d29968
yinm <yinm.cminor9@gmail.com>
2019-01-01 16:45:09 +0900

    Connect MongoDB

---
+++ ch08/8.3.2/app.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var
+  http = require('http'),
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  logger = require('morgan'),
+  errorHandler = require('errorhandler'),
+  routes = require('./routes'),
+
+  app = express(),
+  server = http.createServer(app)
+
+app.use(bodyParser.json())
+app.use(methodOverride('X-HTTP-Method-Override'))
+app.use(express.static(__dirname + '/public'))
+app.use(errorHandler())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('combined'))
+}
+
+routes.configRoutes(app, server)
+
+server.listen(3000)
+console.log(
+  'Express server listening on port %d in %s mode',
+  server.address().port,
+  app.settings.env
+)
